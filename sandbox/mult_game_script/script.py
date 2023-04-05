138bd7787923a93afad02ca1c6057806f00bd6a6
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-04-05 21:17:25 +0200

    mult game : + color

--- sandbox/mult_game_script/script.py
+++
@@ -1,8 +0,0 @@
-import random
-
-while True:
-    a, b = random.randint(11, 99), random.randint(11, 99)
-    res = a * b
-    guess = int(input(f'can you gimme the answer to {a} * {b}? \n\n'))
-    print(f'{guess} is right!\n') if res == guess else print(f'{guess} is wrong!\n')
-    
