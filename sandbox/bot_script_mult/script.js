c222dfa4bcd70472cbe48efa3d155fde0196faf0
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-04-05 21:39:56 +0200

    mult game : js fixed

--- sandbox/bot_script_mult/script.js
+++ sandbox/bot_script_mult/script.js
@@ -1,24 +1,16 @@
-const prompt = require('prompt-sync')();
-// const prompt = require('prompt-sync')({sigint: true});
+const prompt = require('prompt-sync')({sigint: true});
 
-process.on('SIGINT', () => {
-  console.log('\n(exiting ...)')
-  process.exit()
-})
-
-	console.log('can you gimme the answers to the problems below? \n\n');
 while (1) {
 	let a = parseInt(Math.random() * 10) + 1
 	let b = parseInt(Math.random() * 10) + 1
+
+	console.log('Can you solve this:', a, 'x', b)
+
 	let res = a * b
 
-	console.log(a + ' x ' + b)
-	let guess = prompt('\n');
-
+	let guess = prompt('Can you gimme the answer? ');
 	guess = parseInt( guess )
-	if (isNaN(guess)) {
-		console.log("invalid input \n")
-	} else if (res == guess) {
+	if (res == guess) {
 		console.log(`${guess}! right! \n`)
 	} else {
 		console.log(`${guess}! wrong! (correct answer is ${res})\n`)
