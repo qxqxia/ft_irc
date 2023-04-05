bd696f318ce9a4c8ade31b77350d183d902144c0
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-04-05 22:43:37 +0200

    user : hpp

--- project/User.hpp
+++ project/User.hpp
@@ -1,12 +1,40 @@
-#pragma once
-#ifndef __USER_HPP__
-# define __USER_HPP__
+using	namespace std;
 
 class	User
 {
 
 private:
-public:
+	string	m_nickname;
+	string	m_username;
+	string	m_hostname;
+	string	m_realname;
+	string	m_mode;
 
-}
+	set<string> m_channels;
+
+public:
+	User(
+		string nick,
+		string user,
+		string hostname,
+		string realname
+	);
+
+	~User();
+
+	string	get_nickname() const;
+	string	get_username() const;
+	string	get_hostname() const;
+	string	get_realname() const;
+
+	set<string>	& get_channels();
+
+	string	get_mode() const;
+
+};
+
+
+#endif
+
+
 
