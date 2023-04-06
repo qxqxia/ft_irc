8b79cf3840b01ca044cb409a0d5df22b4092d2a1
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-04-06 08:50:07 +0200

    snprintf : sandbox done

---
+++ sandbox/snprintf/main.cc
@@ -0,0 +1,24 @@
+#include "iostream"
+
+int	main()
+{
+	{
+		char	buff[1024];
+		int	n = 42;
+
+		snprintf(buff, sizeof(buff), "let's print this number %d ", n);
+		std::cout << buff << '\n';
+	}
+	/*{
+		char	buff[1024];
+		int	num = 21;
+
+		snprintf(
+			buff,
+			sizeof(buff),
+			"let's print this number: %d ",
+			num
+		);
+		printf("%s \n", buffer);
+	}*/
+}
