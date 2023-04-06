7d65ae22218d0bedad1ddf9ae5f03cd5fd30c698
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-04-06 09:07:14 +0200

    run py using system : looks good, + comments

--- sandbox/system_python_hello_world/main.cc
+++ sandbox/system_python_hello_world/main.cc
@@ -29,10 +29,26 @@ int	main()
 		return 0;
 	}
 
+	// FILE *fp is a pointer to a FILE object, which is a file stream
+	//  fp is used to capture the output of the executed python command 
+	//  by calling popen() with the complete command as an argument 
+	//  to open a pipe to the stdout of the executed command
+	//  The resulting FILE object is then used to read the output
+
 	while (fgets(buff, sizeof(buff), fp) != nullptr)
 	{
 		cout << buff;
 	}
+
+	// fgets reads a line from a file stream and stores it in a buffer
+	//  here fgets() reads the output of the executed python command
+	//  one line at a time from the FILE object fp
+	//  then stores it in the buff
+	//  once done, we std::cout the contents of the buffer
+	//
+	//  The loop goes on until the end of the output stream
+	//  fgets() returning nullptr by that time
+
 
 	pclose(fp);
 }
