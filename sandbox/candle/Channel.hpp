18c8ffd64c9bd8f8918a8cc8611fcdb8f723163a
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-04-17 20:05:07 +0200

    candle : moved here

---
+++ sandbox/candle/Channel.hpp
@@ -0,0 +1,75 @@
+#pragma once
+#ifndef __CHANNEL_HPP__
+# define __CHANNEL_HPP__
+
+# include "Freenode.hpp"
+using	namespace std;
+
+class	User;
+
+class	Channel
+{
+
+private:
+
+	map<int, User*>	m_users;
+	map<int, User*>	m_chanops;
+	map<int, User*>	m_voices;
+	string		m_channelname;
+	string		m_topic;
+	string		m_mode;
+	string		m_key;
+
+	map<string, string>	m_banlist;
+
+	int		m_maximum_users;
+	// time_t		m_last_joke_told;
+
+
+public:
+	Channel(string);
+	~Channel();
+
+	map<string, string>
+		& get_banlist();
+
+	map<int, User*>	& get_users();
+	map<int, User*>	& get_chanops();
+	map<int, User*>	& get_voices();
+
+	string	get_topic() const;
+	string	get_channelname() const;
+	string	get_mode() const;
+	string	get_key() const;
+
+	string get_list_of_users_in_channel();
+	string get_list_of_users_banned();
+
+	int	get_user_number() const;
+	int	get_maximum_users() const;
+
+	// time_t	get_last_joke_told() const;
+
+	bool	is_chanop(int sd);
+	bool	is_voice(int sd);
+	bool	is_banned(string nick);
+
+	void	set_topic(string top);
+	void	set_mode(string mod);
+	void	set_key(string key);
+	void	set_maximum_users(int max);
+	// void	set_last_joke_told();
+
+	void	add_user(int sd, User *u);
+	void	add_chanop(int sd, User *u);
+	void	add_voice(int sd, User *u);
+
+	void	left_user_of_what_use(int sd);
+	int	search_user_by_nickname(string nick);
+
+};
+
+
+#endif
+
+
