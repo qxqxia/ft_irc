68b357735e7d72a23d49aa5e5cda82f9b79c5f47
uxn <malcolm.nuo.xu@gmail.com>
2023-05-11 10:00:14 +0200

    + debugger in Sender RPL ERR

--- project/forward_rpl_err.cpp
+++ project/forward_rpl_err.cpp
@@ -10,125 +10,132 @@ std::string get_RPL_ERR(int code, Server *serv, User *user, std::string arg1, st
     else if (code < 100)
         code_in_string.insert(0, 1, '0');
 
-    std::string ret;
+    std::string res;
 
     if (user)
     {
-        ret += ":" + serv->get_server_name() + " " + code_in_string + " " + user->get_nickname() + " ";
+        res += ":" + serv->get_server_name() + " " + code_in_string + " " + user->get_nickname() + " ";
     }
     else
     {
-        ret += ":" + serv->get_server_name() + " " + code_in_string + " " + "*" + " ";
+        res += ":" + serv->get_server_name() + " " + code_in_string + " " + "*" + " ";
     }
     switch (code)
     {
         case 001:
-            ret += RPL_WELCOME(user->get_nickname(), user->get_username(), user->get_hostname());
+            res += RPL_WELCOME(user->get_nickname(), user->get_username(), user->get_hostname());
 			break ;
 		case 002:
-            ret += RPL_YOURHOST;
+            res += RPL_YOURHOST;
 			break ;
         case 003:
-            ret += RPL_CREATED;
+            res += RPL_CREATED;
 			break ;
         case 004:
-            ret += RPL_MYINFO;
+            res += RPL_MYINFO;
 			break ;
         case 005:
-            ret += RPL_BOUNCE(serv->get_port());
+            res += RPL_BOUNCE(serv->get_port());
 			break ;
         case 221:
-            ret += RPL_UMODEIS(arg1);
+            res += RPL_UMODEIS(arg1);
 			break ;
         case 324:
-            ret += RPL_CHANNELMODEIS(arg1, arg2);
+            res += RPL_CHANNELMODEIS(arg1, arg2);
 			break ;
         case 331:
-            ret += RPL_NOTOPIC(arg1);
+            res += RPL_NOTOPIC(arg1);
 			break ;
         case 332:
-            ret += RPL_TOPIC(arg1, arg2);
+            res += RPL_TOPIC(arg1, arg2);
 			break ;
         case 353:
-            ret += RPL_NAMREPLY(arg1, arg2);
+            res += RPL_NAMREPLY(arg1, arg2);
 			break ;
         case 366:
-            ret += RPL_ENDOFNAMES(arg1);
+            res += RPL_ENDOFNAMES(arg1);
 			break ;
         case 367:
-            ret += RPL_BANLIST(arg1, arg2);
+            res += RPL_BANLIST(arg1, arg2);
 			break ;
         case 368:
-            ret += RPL_ENDOFBANLIST(arg1);
+            res += RPL_ENDOFBANLIST(arg1);
 			break ;
         case 381:
-            ret += RPL_YOUREOPER;
+            res += RPL_YOUREOPER;
 			break ;
         case 401:
-            ret += ERR_NOSUCHNICK(arg1);
+            res += ERR_NOSUCHNICK(arg1);
 			break ;
         case 403:
-            ret += ERR_NOSUCHCHANNEL(arg1);
+            res += ERR_NOSUCHCHANNEL(arg1);
 			break ;
         case 404:
-            ret += ERR_CANNOTSENDTOCHAN(arg1);
+            res += ERR_CANNOTSENDTOCHAN(arg1);
 			break ;
         case 405:
-            ret += ERR_TOOMANYCHANNELS(arg1);
+            res += ERR_TOOMANYCHANNELS(arg1);
             break ;
         case 431:
-            ret += ERR_NONICKNAMEGIVEN;
+            res += ERR_NONICKNAMEGIVEN;
 			break ;
         case 432:
-            ret += ERR_ERRONEUSNICKNAME(arg1);
+            res += ERR_ERRONEUSNICKNAME(arg1);
 			break ;
         case 433:
-            ret += ERR_NICKNAMEINUSE(arg1);
+            res += ERR_NICKNAMEINUSE(arg1);
 			break ;
         case 441:
-            ret += ERR_USERNOTINCHANNEL(arg1, arg2);
+            res += ERR_USERNOTINCHANNEL(arg1, arg2);
 			break ;
         case 442:
-            ret += ERR_NOTONCHANNEL(arg1);
+            res += ERR_NOTONCHANNEL(arg1);
 			break ;
         case 461:
-            ret += ERR_NEEDMOREPARAMS(arg1);
+            res += ERR_NEEDMOREPARAMS(arg1);
 			break ;
         case 464:
-            ret += ERR_PASSWDMISMATCH;
+            res += ERR_PASSWDMISMATCH;
 			break ;
         case 471:
-            ret += ERR_CHANNELISFULL(arg1);
+            res += ERR_CHANNELISFULL(arg1);
 			break ;
         case 472:
-            ret += ERR_UNKNOWNMODE(arg1, arg2);
+            res += ERR_UNKNOWNMODE(arg1, arg2);
             break ;
         case 473:
-            ret += ERR_INVITEONLYCHAN(arg1);
+            res += ERR_INVITEONLYCHAN(arg1);
 			break ;
         case 474:
-            ret += ERR_BANNEDFROMCHAN(arg1);
+            res += ERR_BANNEDFROMCHAN(arg1);
 			break ;
         case 475:
-            ret += ERR_BADCHANNELKEY(arg1);
+            res += ERR_BADCHANNELKEY(arg1);
 			break ;
         case 481:
-            ret += ERR_NOPRIVILEGES;
+            res += ERR_NOPRIVILEGES;
             break ;
         case 482:
-            ret += ERR_CHANOPRIVSNEEDED(arg1);
+            res += ERR_CHANOPRIVSNEEDED(arg1);
 			break ;
         case 484:
-            ret += ERR_RESTRICTED;
+            res += ERR_RESTRICTED;
 			break ;
         case 501:
-            ret += ERR_UMODEUNKNOWNFLAG;
+            res += ERR_UMODEUNKNOWNFLAG;
 			break ;
         case 669:
-            ret += BOT_NOCOMMAMD(arg1);
+            res += BOT_NOCOMMAMD(arg1);
             break;
 		default:
-			;
+		    /**/;
     }
-    return ret;
+
+
+    // std::cout << "(dbg) get_RPL_ERR: " << res << std::endl;
+    std::cout << CYAN "(dbg)(get_RPL_ERR): " RESET << (
+        (res.empty()) ? "(None)" : res
+    );
+
+    return res;
 }
