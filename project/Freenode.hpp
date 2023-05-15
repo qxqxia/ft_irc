e291ee08cc4a3676ec778b03e83b20e6b95f9246
uxn <malcolm.nuo.xu@gmail.com>
2023-05-15 09:35:16 +0200

    minor

--- project/Freenode.hpp
+++ project/Freenode.hpp
@@ -42,7 +42,7 @@
 # define OPER_PASS "1007"
 
 # define USER_MODE \
-				"ro" // "roi"
+				"roi" // "roi"
 # define CHANNEL_MODE \
 				"mvblo" // "maltvonkb"
 
--- project/commands/ping.cpp
+++ project/commands/ping.cpp
@@ -5,7 +5,7 @@ void ping(Server *serv, std::string buffer, int sd)
     (void)  serv;
 
     buffer[1] = 'O';
-    Broadcast(buffer, sd);
+    // Broadcast(buffer, sd);
     Broadcast(buffer, sd);
     /*
     
